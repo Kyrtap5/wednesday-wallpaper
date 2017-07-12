ba12c6939ffa8942ddb482f1630c22c4f7c1e832
Kyrtap5 <Kyrtap5@users.noreply.github.com>
2017-07-12 13:13:57 +0200

    Added project files

---
+++ app/src/main/java/de/kyrtap5/wednesdaywallpaper/MainActivity.java
@@ -0,0 +1,13 @@
+package de.kyrtap5.wednesdaywallpaper;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
