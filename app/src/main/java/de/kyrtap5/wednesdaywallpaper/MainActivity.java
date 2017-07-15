fd8d8b865a84d7756723f643fb74ddbcd52e767d
Kyrtap5 <Kyrtap5@users.noreply.github.com>
2017-07-15 19:16:25 +0200

    Added switch to ActionBar and implemented alarm cancelling

--- app/src/main/java/de/kyrtap5/wednesdaywallpaper/MainActivity.java
+++ app/src/main/java/de/kyrtap5/wednesdaywallpaper/MainActivity.java
@@ -7,6 +7,9 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 
 import java.util.Calendar;
 
@@ -14,6 +17,9 @@ public class MainActivity extends AppCompatActivity {
     private SharedPreferences sPrefs = null;
     private BackgroundChanger bChanger;
     private ImageHandler iHandler;
+    private AlarmManager aManager;
+    private PendingIntent pIntent;
+    private Calendar calendar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,32 +29,48 @@ public class MainActivity extends AppCompatActivity {
         iHandler = new ImageHandler(this);
         //Initialize SharedPreferences
         sPrefs = this.getSharedPreferences("de.kyrtap5.wednesdaywallpaper", MODE_PRIVATE);
+        calendar = Calendar.getInstance();
+        calendar.set(Calendar.HOUR_OF_DAY, 0);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
+        pIntent = PendingIntent.getBroadcast(this, 0, new Intent(this, AlarmBroadcastReceiver.class),PendingIntent.FLAG_UPDATE_CURRENT);
+        aManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        Switch mainSwitch = (Switch) findViewById(R.id.mainSwitch);
+        mainSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) setupAlarm();
+                else cancelAlarm();
+            }
+        });
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        checkFirstRun();
+        iHandler.saveDrawable(bChanger.getBackground(), "images", "wallpaper.png");
         setupAlarm();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
     }
 
     private boolean checkFirstRun() {
         //Check SharedPreferences for firstrun attribute
         if (sPrefs.getBoolean("firstrun", true)) {
             //App started for the first time: save current wallpaper
-            iHandler.saveDrawable(bChanger.getBackground(), "images", "wallpaper.png");
             sPrefs.edit().putBoolean("firstrun", false).commit();
             return true;
         } else return false;
     }
 
     private void setupAlarm() {
-        Calendar calendar = Calendar.getInstance();
-        calendar.set(Calendar.HOUR_OF_DAY, 0);
-        calendar.set(Calendar.MINUTE, 0);
-        calendar.set(Calendar.SECOND, 0);
-        PendingIntent pIntent = PendingIntent.getBroadcast(this, 0, new Intent(this, AlarmBroadcastReceiver.class),PendingIntent.FLAG_UPDATE_CURRENT);
-        AlarmManager aManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
         aManager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), AlarmManager.INTERVAL_DAY, pIntent);
+    }
+
+    private void cancelAlarm() {
+        aManager.cancel(pIntent);
     }
 }
